b3297e79449c7b09d702483642d9f73de6308650
george-scott-code <george.scott.code@gmail.com>
2025-02-11 13:36:12 +0000

    return the last value that was set for a given key

--- KeyValueStore.api/Store/TextStore.cs
+++ KeyValueStore.api/Store/TextStore.cs
@@ -16,32 +16,31 @@ public class TextStore : IKeyValueStore
         using FileStream fs = new(dbPath, FileMode.Open, FileAccess.Read);
         using StreamReader sw = new StreamReader(fs);
 
+        string? value = null;
         string? line;
-        // todo: we should be looking for the last occurance
+        
+        // todo: parses whole file to match last occurance, consider reading the file in reverse when switching to a binary format
         while ((line = sw.ReadLine()) != null)
         {
-            var parts = line.Split(" ");
+            var parts = line.Split(',');
             if (parts[0] == key)
             {
-                return parts[1];
+                value = parts[1];
             }
         }
-        return null;
+        return value;
     }
 
+    // todo: should the key be an int or guid? does it need to be specified or returned?
     public void Set(string key, string value)
     {
         // todo: sanitize string
         string dbPath = "D:\\source\\KeyValueStore\\db.txt";
-        FileStream fileStream = File.Open(dbPath, FileMode.Open);
-
-        fileStream.SetLength(0);
-        fileStream.Close();
 
         using FileStream fs = new(dbPath, FileMode.Append);
         using StreamWriter sw = new(fs);
 
         // todo: write, format, csv
-        sw.WriteLine($"{key} {value}");
+        sw.WriteLine($"{key},{value}");
     }
 }
--- KeyValueStore.test/KeyValueStoreTests.cs
+++ KeyValueStore.test/KeyValueStoreTests.cs
@@ -3,9 +3,17 @@ namespace KeyValueStore;
 
 public class KeyValueStoreTests
 {
+
+    // todo: database provider / configuration
     [Fact]
     public void Test1()
     {
+        string dbPath = "D:\\source\\KeyValueStore\\db.txt";
+        FileStream fileStream = File.Open(dbPath, FileMode.Open);
+
+        fileStream.SetLength(0);
+        fileStream.Close();
+
         var store = new InMemoryStore();
 
         store.Set("Hello", "World");
@@ -17,12 +25,36 @@ public class KeyValueStoreTests
     [Fact]
     public void Test2()
     {
+        string dbPath = "D:\\source\\KeyValueStore\\db.txt";
+        FileStream fileStream = File.Open(dbPath, FileMode.Open);
+
+        fileStream.SetLength(0);
+        fileStream.Close();
+
         var store = new TextStore();
 
         store.Set("Hello", "World");
 
         var result = store.Get("Hello");
         Assert.Equal("World", result);
+    }
+
+    [Fact]
+    public void WhenTheSameKeyHasBeenSetTwice_ReturnTheLatestValue()
+    {
+        string dbPath = "D:\\source\\KeyValueStore\\db.txt";
+        FileStream fileStream = File.Open(dbPath, FileMode.Open);
+
+        fileStream.SetLength(0);
+        fileStream.Close();
+
+        var store = new TextStore();
+
+        store.Set("Hello", "World");
+        store.Set("Hello", "Dog");
+
+        var result = store.Get("Hello");
+        Assert.Equal("Dog", result);
     }
 
 }
