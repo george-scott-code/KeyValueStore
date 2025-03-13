690283b3cbd27287a19b15826bfb528f301f3114
george-scott-code <george.scott.code@gmail.com>
2025-03-13 20:25:55 +0000

    use top level program.cs

--- KeyValueStore.api/Program.cs
+++ KeyValueStore.api/Program.cs
@@ -1,52 +1,45 @@
 using KeyValueStore.api.Data;
 using KeyValueStore.api.Store;
 
-internal class Program
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddSingleton<IFileProvider, FileProvider>();
+builder.Services.AddSingleton<IndexedTextStore, IndexedTextStore>();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
 {
-    private static void Main(string[] args)
-    {
-        var builder = WebApplication.CreateBuilder(args);
-
-        builder.Services.AddEndpointsApiExplorer();
-        builder.Services.AddSwaggerGen();
-
-        builder.Services.AddSingleton<IFileProvider, FileProvider>();
-        builder.Services.AddSingleton<IndexedTextStore, IndexedTextStore>();
-
-        var app = builder.Build();
-
-        // Configure the HTTP request pipeline.
-        if (app.Environment.IsDevelopment())
-        {
-            app.UseSwagger();
-            app.UseSwaggerUI();
-        }
-
-        app.UseHttpsRedirection();
-
-        var indexedTextStore = app.Services.GetService<IndexedTextStore>() ?? throw new ArgumentNullException();
-        
-        app.MapGet("/value", (string key) =>
-        {
-            return indexedTextStore.Get(key);
-        })
-        .WithName("GetValue")
-        .WithOpenApi();
-
-        app.MapPost("/value", (string key, string value) =>
-        {
-            indexedTextStore.Set(key, value);
-        })
-        .WithName("PostValue")
-        .WithOpenApi();
-
-        app.MapDelete("/value", (string key) =>
-        {
-            indexedTextStore.Remove(key);
-        })
-        .WithName("Remove")
-        .WithOpenApi();
-
-        app.Run();
-    }
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
+
+app.UseHttpsRedirection();
+
+var indexedTextStore = app.Services.GetService<IndexedTextStore>() ?? throw new ArgumentNullException();
+
+app.MapGet("/value", (string key) =>
+{
+    return indexedTextStore.Get(key);
+})
+.WithName("GetValue")
+.WithOpenApi();
+
+app.MapPost("/value", (string key, string value) =>
+{
+    indexedTextStore.Set(key, value);
+})
+.WithName("PostValue")
+.WithOpenApi();
+
+app.MapDelete("/value", (string key) =>
+{
+    indexedTextStore.Remove(key);
+})
+.WithName("Remove")
+.WithOpenApi();
+
+app.Run();
