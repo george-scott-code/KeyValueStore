9c667323ac925f3596ef1e905b6d4264223ac646
george-scott-code <george.scott.code@gmail.com>
2025-03-13 20:11:57 +0000

    comments

--- KeyValueStore.api/Program.cs
+++ KeyValueStore.api/Program.cs
@@ -7,8 +7,6 @@ internal class Program
     {
         var builder = WebApplication.CreateBuilder(args);
 
-        // Add services to the container.
-        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
         builder.Services.AddEndpointsApiExplorer();
         builder.Services.AddSwaggerGen();
 
